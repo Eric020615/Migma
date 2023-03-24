691acc3fed48e3522bb14284ca0e264786aa6d56
ericj <ericjuncheng10@gmail.com>
2023-03-24 22:16:23 +0800

    user can start to register and data will be stored in firebase

---
+++ app/src/main/java/com/example/migma/StartingActivity.java
@@ -0,0 +1,40 @@
+package com.example.migma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+
+public class StartingActivity extends AppCompatActivity {
+
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.starting_activity);
+        Button LoginBtn = findViewById(R.id.start_login_button);
+        Button SignUpBtn = findViewById(R.id.start_signup_button);
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(buttonClick);
+                Intent intent = new Intent(StartingActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        SignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(buttonClick);
+                Intent intent = new Intent(StartingActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
